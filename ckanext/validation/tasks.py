560870625377b46b65f6d3a975f2643ca78afe57
Mikel Emaldi <m.emaldi@gmail.com>
2015-08-31 10:36:37 +0200

    Fixed #1

--- ckanext/validation/tasks.py
+++ ckanext/validation/tasks.py
@@ -29,7 +29,8 @@ CSV_FORMAT = ['csv', 'text/comma-separated-values', 'text/csv', 'application/csv
 RDF_FORMAT = ['rdf', 'application/rdf+xml', 'text/plain', 'application/x-turtle', 'text/rdf+n3']
 
 def validate_xml(validation, url):
-    r = requests.get(url)
+    headers = {'Authorization': API_KEY}
+    r = requests.get(url, headers=headers)
     schema_root = etree.XML(validation)
     schema = etree.XMLSchema(schema_root)
     parser = etree.XMLParser(schema = schema)
@@ -42,7 +43,8 @@ def validate_xml(validation, url):
         return False, errors
 
 def validate_json(validation, url):
-    r = requests.get(url)
+    headers = {'Authorization': API_KEY}
+    r = requests.get(url, headers=headers)
     schema_dict = eval(validation)
     errors = []
     try:
@@ -53,7 +55,8 @@ def validate_json(validation, url):
         return False, errors
 
 def validate_csv(validation, url):
-    csv_text = requests.get(url).content
+    headers = {'Authorization': API_KEY}
+    csv_text = requests.get(url, headers=headers).content
     api_url = WELIVE_API + 'validation/csv'
     files = {'csv': csv_text, 'schema': validation}
     r = requests.post(api_url, files=files)
@@ -64,12 +67,12 @@ def validate_csv(validation, url):
         return False, errors
 
 def validate_rdf(url):
-    rdf_text = requests.get(url).content
+    headers = {'Authorization': API_KEY}
+    rdf_text = requests.get(url, headers=headers).content
     api_url = WELIVE_API + 'validation/rdf'
     files = {'rdf': rdf_text}
     r = requests.post(api_url, files=files)
     errors = r.json()['errors']
-    print errors
     if r.json()['result'] == 'true':
         return True, errors
     else:
@@ -79,50 +82,57 @@ def validate_rdf(url):
 @periodic_task(run_every=timedelta(seconds=5))
 def validate():
     res = requests.get(
-        API_URL + 'action/package_list',
+        API_URL + 'action/organization_list',
         headers = {'Authorization': API_KEY,
                    'Content-Type': 'application/json'}
     )
-    package_list = res.json()['result']
-
-    for package in package_list:
+    organization_list = res.json()['result']
+    for organization_name in organization_list:
         res = requests.post(
-            API_URL + 'action/package_show', json.dumps({'id': package}),
+            API_URL + 'action/organization_show', json.dumps({'id': organization_name}),
             headers = {'Authorization': API_KEY,
                        'Content-Type': 'application/json'}
         )
-        package = res.json()
-        if 'resources' in package['result']:
-            for resource in package['result']['resources']:
-                # TODO: Check format!!!
-                if resource['format'].lower() in JSON_FORMAT or resource['format'].lower() in XML_FORMAT or resource['format'].lower() in CSV_FORMAT or resource['format'].lower() in RDF_FORMAT:
-                    last = None
-                    last_validation = datetime(1970, 01, 01)
-                    if resource['update_time'] == None:
-                        last = dateutil.parser.parse(resource['created'])
-                    else:
-                        last = dateutil.parser.parse(resource['update_time'])
-                    if 'validation_time' in resource:
-                        last_validation = dateutil.parser.parse(resource['validation_time'])
-                    errors = None
-                    if (last > last_validation):
-                        validation = False
-                        if resource['format'].lower() in RDF_FORMAT:
-                            validation, errors = validate_rdf(resource['url'])
-                        elif resource['format'].lower() in JSON_FORMAT and ('validation' in resource):
-                            validation, errors = validate_json(resource['validation'], resource['url'])
-                        elif resource['format'].lower() in XML_FORMAT and ('validation' in resource):
-                            validation, errors = validate_xml(resource['validation'], resource['url'])
-                        elif resource['format'].lower() in CSV_FORMAT and ('validation' in resource):
-                            validation, errors = validate_csv(resource['validation'], resource['url'])
-                        resource['validated'] = validation
-                        resource['validation_time'] = str(datetime.now())
-                        if errors != None:
-                            resource['validation_errors'] = str(errors)
-                        else:
-                            resource['validation_errors'] = ""
-                        res = requests.post(
-                            API_URL + 'action/resource_update', json.dumps(resource),
-                            headers = {'Authorization': API_KEY,
-                                       'Content-Type': 'application/json'}
-                        )
+        organization = res.json()
+        if 'packages' in organization['result']:
+            for package_res in organization['result']['packages']:
+                package_name = package_res['name']
+                package = requests.post(
+                    API_URL + 'action/package_show', json.dumps({'id': package_name}),
+                    headers = {'Authorization': API_KEY,
+                               'Content-Type': 'application/json'}
+                )
+                if 'resources' in package.json()['result']:
+                    for resource in package.json()['result']['resources']:
+                        # TODO: Check format!!!
+                        if resource['format'].lower() in JSON_FORMAT or resource['format'].lower() in XML_FORMAT or resource['format'].lower() in CSV_FORMAT or resource['format'].lower() in RDF_FORMAT:
+                            last = None
+                            last_validation = datetime(1970, 01, 01)
+                            if resource['update_time'] == None:
+                                last = dateutil.parser.parse(resource['created'])
+                            else:
+                                last = dateutil.parser.parse(resource['update_time'])
+                            if 'validation_time' in resource:
+                                last_validation = dateutil.parser.parse(resource['validation_time'])
+                            errors = None
+                            if (last > last_validation):
+                                validation = False
+                                if resource['format'].lower() in RDF_FORMAT:
+                                    validation, errors = validate_rdf(resource['url'])
+                                elif resource['format'].lower() in JSON_FORMAT and ('validation' in resource):
+                                    validation, errors = validate_json(resource['validation'], resource['url'])
+                                elif resource['format'].lower() in XML_FORMAT and ('validation' in resource):
+                                    validation, errors = validate_xml(resource['validation'], resource['url'])
+                                elif resource['format'].lower() in CSV_FORMAT and ('validation' in resource):
+                                    validation, errors = validate_csv(resource['validation'], resource['url'])
+                                resource['validated'] = validation
+                                resource['validation_time'] = str(datetime.now())
+                                if errors != None:
+                                    resource['validation_errors'] = str(errors)
+                                else:
+                                    resource['validation_errors'] = ""
+                                res = requests.post(
+                                    API_URL + 'action/resource_update', json.dumps(resource),
+                                    headers = {'Authorization': API_KEY,
+                                               'Content-Type': 'application/json'}
+                                )
